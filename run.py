65dcbb4e7e217ae536908382258686cdcbf9d81a
Richard Lin <yifengl@stanford.edu>
2020-02-09 17:19:25 -0800

    [setup] Initialize all files
    * Logger.py is downloaded from a public repo to help with managing
      tensorboard in pytorch

---
+++ run.py
@@ -0,0 +1,31 @@
+import numpy as np
+import argparse
+
+def argParser():
+	"""
+	"""
+
+
+def train():
+	"""
+	"""
+	pass
+
+
+def test():
+	"""
+	"""
+	pass
+
+
+def main():
+	"""
+	"""
+	pass
+
+
+
+
+
+if __name__ == "__main__":
+	main()
