4fd55c3eb4eaca9ca86ba6a19a66095fe0ecf521
Noa Bendit-Shtull <noa.f.bs@gmail.com>
2020-02-27 13:57:59 -0800

    [infra] Reorganize baseline model
    - Move small functions to utils
    - Extract training and validation from Trainer() class
    - Create separate function to build TEXT object

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ import torch.nn.functional as F
 from torch.autograd import Variable
 from tqdm import tqdm
 from model import LSTMLanguageModel
-
+import utils
 
 def argParser():
     """
@@ -35,154 +35,101 @@ def argParser():
     args = parser.parse_args()
     return args
 
+def build_dataset(args):
+    TEXT = torchtext.data.Field()
+    dataset = torchtext.datasets.LanguageModelingDataset.splits(
+        path=".", 
+        train=args.train_path, 
+        validation=args.valid_path, 
+        test=args.valid_path, 
+        text_field=TEXT)
 
-def train():
-    """
-    """
-    pass
+    TEXT.build_vocab(train, max_size=1000) if False else TEXT.build_vocab(train)
+    TEXT.vocab.load_vectors('glove.840B.300d')
+    train_iter, val_iter, test_iter = torchtext.data.BPTTIterator.splits(
+        dataset, 
+        batch_size=args.batch_size, 
+        device=device, 
+        bptt_len=args.bptt_len, 
+        repeat=False)
+
+    self.train_iter = train_iter
+    self.val_iter = val_iter
+    return TEXT, dataset
 
 
-def test():
-    """
-    """
-    pass
-
-
-class Trainer:
-    def __init__(self, args, device):
-
-        self.device =  device
-        self.TEXT = self.build_dataset(args)
-
-        self.model = LSTMLanguageModel(
-            TEXT = self.TEXT,
-            hidden_dim = args.hidden_size, 
-            batch_size = args.batch_size, 
-            dropout_rate=args.dropout)
-
-        self.model = self.model.to(self.device)
-
-    def build_dataset(self, args):
-        TEXT = torchtext.data.Field()
-        train, val, test = torchtext.datasets.LanguageModelingDataset.splits(
-            path=".", 
-            train=args.train_path, 
-            validation=args.valid_path, 
-            test=args.valid_path, 
-            text_field=TEXT)
-
-        TEXT.build_vocab(train, max_size=1000) if False else TEXT.build_vocab(train)
-        TEXT.vocab.load_vectors('glove.840B.300d')
-        train_iter, val_iter, test_iter = torchtext.data.BPTTIterator.splits(
-            (train, val, test), 
-            batch_size=args.batch_size, 
-            device=self.device, 
-            bptt_len=args.bptt_len, 
-            repeat=False)
-
-        self.train_iter = train_iter
-        self.val_iter = val_iter
-        return TEXT
-
-    def string_to_batch(self, string):
-        relevant_split = string.split() # last two words, ignore ___
-        ids = [self.word_to_id(word) for word in relevant_split]
-        return Variable(torch.LongTensor(ids, device = self.device))
-        
-    def word_to_id(self, word):
-        return self.TEXT.vocab.stoi[word]
+def train(num_epochs):
+    parameters = filter(lambda p: p.requires_grad, self.model.parameters())
+    optimizer = torch.optim.Adam(params = parameters, lr=3e-4)
+    criterion = nn.NLLLoss()
     
-    def batch_to_input(self, batch):
-        ngrams = self.collect_batch_ngrams(batch)
-        x = Variable(torch.LongTensor([ngram[:-1] for ngram in ngrams], device = self.device))
-        y = Variable(torch.LongTensor([ngram[-1] for ngram in ngrams], device = self.device))
-        return x, y
-    
-    def collect_batch_ngrams(self, batch, n = 3):
-        data = batch.text.view(-1).data.tolist()
-        return [tuple(data[idx:idx + n]) for idx in range(0, len(data) - n + 1)]
-    
-    def train_model(self, num_epochs):
-        parameters = filter(lambda p: p.requires_grad, self.model.parameters())
-        optimizer = torch.optim.Adam(params = parameters, lr=1e-3)
-        criterion = nn.NLLLoss()
-        
-        for epoch in tqdm(range(num_epochs)):
+    for epoch in tqdm(range(num_epochs)):
 
-            epoch_loss = []
-            hidden = self.model.init_hidden()
-            self.model.train()
-
-            for batch in tqdm(self.train_iter):
-                x, y = batch.text, batch.target.view(-1)
-
-                optimizer.zero_grad()
-
-                y_pred, hidden = self.model.forward(x, hidden, train = True)
-
-                loss = criterion(y_pred, y)
-                loss.backward()
-
-                torch.nn.utils.clip_grad_norm_(self.model.lstm.parameters(), 1)
-
-                optimizer.step()
-
-                epoch_loss.append(loss.item())
-                
-            self.model.eval()
-            train_ppl = np.exp(np.mean(epoch_loss))
-            val_ppl = self.validate()
-
-            print('Epoch {0} | Loss: {1} | Train PPL: {2} | Val PPL: {3}'.format(epoch+1, np.mean(epoch_loss), train_ppl,  val_ppl))
-    
-        print('Model trained.')
-        self.write_kaggle(self.model)
-        print('Output saved.')
-        
-    def validate(self):
-        criterion = nn.NLLLoss()
+        epoch_loss = []
         hidden = self.model.init_hidden()
-        aggregate_loss = []
-        for batch in self.val_iter:
-            y_p, _ = self.model.forward(batch.text, hidden, train = False)
-            y_t = batch.target.view(-1)
+        self.model.train()
+
+        for batch in tqdm(self.train_iter):
+            x, y = batch.text, batch.target.view(-1)
+
+            optimizer.zero_grad()
+
+            y_pred, hidden = self.model.forward(x, hidden, train = True)
+
+            loss = criterion(y_pred, y)
+            loss.backward()
+
+            torch.nn.utils.clip_grad_norm_(self.model.lstm.parameters(), 1)
+
+            optimizer.step()
+
+            epoch_loss.append(loss.item())
             
-            loss = criterion(y_p, y_t)
-            aggregate_loss.append(loss.item())        
-        val_ppl = np.exp(np.mean(aggregate_loss))
-        return val_ppl
-    
-    def predict_sentence(self, string):
-        string = string[:-4]
-        self.model.batch_size = 1
-        hidden = self.model.init_hidden()
-        x = self.string_to_batch(string, self.TEXT)
-        logits, _ = self.model.forward(x, hidden, train = False)
-        argsort_ids = np.argsort(logits[-1].data.tolist())
-        out_ids = argsort_ids[-20:][::-1]
-        out_words = ' '.join([TEXT.vocab.itos[out_id] for out_id in out_ids])
-        return out_words
-    
-    def write_kaggle(self, input_file = 'input.txt'):        
-        inputs = open(input_file, 'r').read().splitlines()
-        outputs = [self.predict_sentence(sentence, self.model, self.TEXT) for sentence in inputs]
-        with open('lstm_output.txt', 'w') as f:
-            f.write('id,word')
-            for idx, line in enumerate(outputs):
-                f.write('\n')
-                f.write(str(idx) + ',')
-                f.write(line) 
+        self.model.eval()
+        train_ppl = np.exp(np.mean(epoch_loss))
+        val_ppl = self.validate()
 
+        print('Epoch {0} | Loss: {1} | Train PPL: {2} | Val PPL: {3}'.format(epoch+1, np.mean(epoch_loss), train_ppl,  val_ppl))
+
+    print('Model trained.')
+    self.write_kaggle(self.model)
+    print('Output saved.')
+        
+def validate():
+    criterion = nn.NLLLoss()
+    hidden = self.model.init_hidden()
+    aggregate_loss = []
+    for batch in self.val_iter:
+        y_p, _ = self.model.forward(batch.text, hidden, train = False)
+        y_t = batch.target.view(-1)
+        
+        loss = criterion(y_p, y_t)
+        aggregate_loss.append(loss.item())        
+    val_ppl = np.exp(np.mean(aggregate_loss))
+    return val_ppl
+    
 
 def main():
     """
     """
+    args = argParser()
+
+    # set device
     use_cuda = torch.cuda.is_available()
     device = 'cuda' if use_cuda else 'cpu'
 
-    args = argParser()
-    trainer = Trainer(args, device)
-    trainer.train_model(num_epochs = args.epochs)
+    # build TEXT object
+    TEXT, dataset = build_dataset(args)
+
+    # build model
+    model = LSTMLanguageModel(
+        TEXT = TEXT,
+        hidden_dim = args.hidden_size, 
+        batch_size = args.batch_size, 
+        dropout_rate=args.dropout)
+    model = model.to(device)
+    
+    train(num_epochs = args.epochs)
 
 if __name__ == "__main__":
     main()
--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "$1" = "train" ]; then
-	CUDA_VISIBLE_DEVICES=0 python3 run.py train --train_path=./data/penn/train.txt --valid_path=./data/penn/valid.txt --hidden-size 1024 
+	CUDA_VISIBLE_DEVICES=0 python3 run.py train --train-path=./data/penn/train.txt --valid-path=./data/penn/valid.txt --hidden-size 1024 
 else
 	echo "Invalid Option Selected"
 fi
