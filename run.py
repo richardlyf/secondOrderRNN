b568b38ff09e8ef945dc7e9788d58a309177d3a2
Noa Bendit-Shtull <noa.f.bs@gmail.com>
2020-02-27 14:15:20 -0800

    [infra] Complete refactoring of baseline model

--- run.py
+++ run.py
@@ -35,9 +35,10 @@ def argParser():
     args = parser.parse_args()
     return args
 
+
 def build_dataset(args):
     TEXT = torchtext.data.Field()
-    dataset = torchtext.datasets.LanguageModelingDataset.splits(
+    train, val, test = torchtext.datasets.LanguageModelingDataset.splits(
         path=".", 
         train=args.train_path, 
         validation=args.valid_path, 
@@ -46,6 +47,11 @@ def build_dataset(args):
 
     TEXT.build_vocab(train, max_size=1000) if False else TEXT.build_vocab(train)
     TEXT.vocab.load_vectors('glove.840B.300d')
+    return TEXT, (train, val, test)
+
+
+def train(model, dataset, TEXT, args, device, num_epochs):
+
     train_iter, val_iter, test_iter = torchtext.data.BPTTIterator.splits(
         dataset, 
         batch_size=args.batch_size, 
@@ -53,54 +59,47 @@ def build_dataset(args):
         bptt_len=args.bptt_len, 
         repeat=False)
 
-    self.train_iter = train_iter
-    self.val_iter = val_iter
-    return TEXT, dataset
-
-
-def train(num_epochs):
-    parameters = filter(lambda p: p.requires_grad, self.model.parameters())
+    parameters = filter(lambda p: p.requires_grad, model.parameters())
     optimizer = torch.optim.Adam(params = parameters, lr=3e-4)
     criterion = nn.NLLLoss()
     
     for epoch in tqdm(range(num_epochs)):
 
         epoch_loss = []
-        hidden = self.model.init_hidden()
-        self.model.train()
+        hidden = model.init_hidden()
+        model.train()
 
-        for batch in tqdm(self.train_iter):
+        for batch in tqdm(train_iter):
             x, y = batch.text, batch.target.view(-1)
 
             optimizer.zero_grad()
 
-            y_pred, hidden = self.model.forward(x, hidden, train = True)
+            y_pred, hidden = model.forward(x, hidden, train = True)
 
             loss = criterion(y_pred, y)
             loss.backward()
 
-            torch.nn.utils.clip_grad_norm_(self.model.lstm.parameters(), 1)
+            torch.nn.utils.clip_grad_norm_(model.lstm.parameters(), 1)
 
             optimizer.step()
 
             epoch_loss.append(loss.item())
             
-        self.model.eval()
+        model.eval()
         train_ppl = np.exp(np.mean(epoch_loss))
-        val_ppl = self.validate()
+        val_ppl = validate(model, val_iter)
 
         print('Epoch {0} | Loss: {1} | Train PPL: {2} | Val PPL: {3}'.format(epoch+1, np.mean(epoch_loss), train_ppl,  val_ppl))
 
     print('Model trained.')
-    self.write_kaggle(self.model)
-    print('Output saved.')
+
         
-def validate():
+def validate(model, val_iter):
     criterion = nn.NLLLoss()
-    hidden = self.model.init_hidden()
+    hidden = model.init_hidden()
     aggregate_loss = []
-    for batch in self.val_iter:
-        y_p, _ = self.model.forward(batch.text, hidden, train = False)
+    for batch in val_iter:
+        y_p, _ = model.forward(batch.text, hidden, train = False)
         y_t = batch.target.view(-1)
         
         loss = criterion(y_p, y_t)
@@ -129,7 +128,7 @@ def main():
         dropout_rate=args.dropout)
     model = model.to(device)
     
-    train(num_epochs = args.epochs)
+    train(model, dataset, TEXT, args, device, num_epochs = args.epochs)
 
 if __name__ == "__main__":
     main()
