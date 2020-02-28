de9878b32162d45319887e9431f1b681cea2ae45
Noa Bendit-Shtull <noa.f.bs@gmail.com>
2020-02-27 18:32:11 -0800

    [infra] Update logger for tensorflow 2.1.0

--- utils.py
+++ utils.py
@@ -13,7 +13,11 @@ def create_unique_logdir(logdir, root_logdir="log/"):
         > create_unique_logdir("baseline_lstm")
         "log/baseline_lstm_Y2020_M2_D27_h16_m5"
     """
+    if logdir == "":
+        return logdir
     localtime = time.localtime(time.time())
     time_label = "Y{}_M{}_D{}_h{}_m{}".format(localtime.tm_year, localtime.tm_mon, \
         localtime.tm_mday, localtime.tm_hour, localtime.tm_min)
-    return os.path.join(root_logdir, logdir + "_" + time_label)
+    unique_logdir = os.path.join(root_logdir, logdir + "_" + time_label)
+    os.makedirs(unique_logdir, exist_ok=True)
+    return unique_logdir
