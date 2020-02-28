eaf4f5d472d8d19135e1186d7781d2288ef96015
Richard Lin <yifengl@stanford.edu>
2020-02-27 23:15:37 -0800

    [infra] Include logger in training loop. Ready for training on VM

--- logger.py
+++ logger.py
@@ -24,8 +24,9 @@ class Logger(object):
 
     def scalar_summary(self, tag, value, step):
         """Log a scalar variable."""
-        summary = tf.Summary(value=[tf.Summary.Value(tag=tag, simple_value=value)])
-        self.writer.add_summary(summary, step)
+        with self.writer.as_default():
+            tf.summary.scalar(tag, value, step=step)
+            self.writer.flush()
 
     def image_summary(self, tag, images, step):
         """Log a list of images."""
--- run.py
+++ run.py
@@ -26,61 +26,80 @@ def argParser():
     """
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--gpu', dest='gpu', default='0', help="The gpu number if there's more than one gpu")
-    parser.add_argument('--log', dest='log', default='', help="unique log directory name under log/. If the name is empty, do not store logs")
-    parser.add_argument('--model', dest='model', default='baseline_lstm', help="name of model to use")
+    parser.add_argument('--gpu', dest='gpu', type=int, default=0, help="The gpu number if there's more than one gpu")
+    parser.add_argument('--log', dest='log', default='', help="Unique log directory name under log/. If the name is empty, do not store logs")
+    parser.add_argument('--log_every', dest='log_every', type=int, default=100, help="Number of itertions between logging to tensorboard within an epoch")
+    parser.add_argument('--lr', dest='lr', type=float, default=3e-4, help="Learning rate for training")
+    parser.add_argument('--model', dest='model', default='baseline_lstm', help="Name of model to use")
     parser.add_argument("--epochs", dest="epochs", type=int, default=10, help="Number of epochs to train for")
     parser.add_argument("--batch-size", dest="batch_size", type=int, default=10, help="Size of the minibatch")
     parser.add_argument("--train-path", dest="train_path", help="Training data file")
     parser.add_argument("--valid-path", dest="valid_path", help="Validation data file")
     parser.add_argument("--bptt-len", dest="bptt_len", default=32, type=int, help="Length of sequences for backpropagation through time")
-    parser.add_argument("--hidden-size", dest="hidden_dim", type=int, default=256, help="dimension fo hidden layer")
-    parser.add_argument("--dropout", dest="dropout_rate", type=float, default=0.3, help="dropout rate")
+    parser.add_argument("--hidden-size", dest="hidden_dim", type=int, default=256, help="Dimension fo hidden layer")
+    parser.add_argument("--dropout", dest="dropout_rate", type=float, default=0.3, help="Dropout rate")
 
     args = parser.parse_args()
     return args
 
 
-def train(model, dataset, TEXT, args, device, num_epochs):
+def train(model, dataset, TEXT, args, device, num_epochs, logger=None):
+    batch_size = args.batch_size
+    backprop_len = args.bptt_len
+    log_every = args.log_every
+    lr = args.lr
+    save_to_log = logger is not None
 
     train_iter, val_iter, test_iter = torchtext.data.BPTTIterator.splits(
         dataset, 
-        batch_size=args.batch_size, 
+        batch_size=batch_size, 
         device=device, 
-        bptt_len=args.bptt_len, 
+        bptt_len=backprop_len, 
         repeat=False)
 
     parameters = filter(lambda p: p.requires_grad, model.parameters())
-    optimizer = torch.optim.Adam(params = parameters, lr=3e-4)
+    optimizer = torch.optim.Adam(params=parameters, lr=lr)
     criterion = nn.NLLLoss()
-    
-    for epoch in tqdm(range(num_epochs)):
 
+    log_step = 0
+    for epoch in tqdm(range(num_epochs)):
         epoch_loss = []
         hidden = model.init_hidden()
         model.train()
 
-        for batch in tqdm(train_iter):
+        for batch_iter, batch in enumerate(tqdm(train_iter)):
             x, y = batch.text, batch.target.view(-1)
-
             optimizer.zero_grad()
-
             y_pred, hidden = model.forward(x, hidden, train=True)
 
             loss = criterion(y_pred, y)
             loss.backward()
 
             torch.nn.utils.clip_grad_norm_(model.lstm.parameters(), 1)
-
             optimizer.step()
-
             epoch_loss.append(loss.item())
+
+            # Add to logger on tensorboard within an epoch
+            if save_to_log and batch_iter % log_every == 0:
+                average_train_loss = np.mean(epoch_loss)
+                average_train_ppl = np.exp(average_train_loss)
+                logger.scalar_summary("average_training_loss", average_train_loss, log_step)
+                logger.scalar_summary("average_training_ppl", average_train_ppl, log_step)
+                log_step += 1
             
         model.eval()
-        train_ppl = np.exp(np.mean(epoch_loss))
-        val_ppl = validate(model, val_iter)
+        epoch_average_loss = np.mean(epoch_loss)
+        epoch_train_ppl = np.exp(epoch_average_loss)
+        epoch_val_ppl = validate(model, val_iter)
 
-        print('Epoch {0} | Loss: {1} | Train PPL: {2} | Val PPL: {3}'.format(epoch+1, np.mean(epoch_loss), train_ppl,  val_ppl))
+        # Add to logger on tensorboard at the end of an epoch
+        if save_to_log:
+            logger.scalar_summary("epoch_training_loss", epoch_average_loss, epoch)
+            logger.scalar_summary("epoch_train_ppl", epoch_train_ppl, epoch)
+            logger.scalar_summary("epoch_val_ppl", epoch_val_ppl, epoch)
+
+        print('Epoch {0} | Loss: {1} | Train PPL: {2} | Val PPL: {3}' \
+            .format(epoch + 1, epoch_average_loss, epoch_train_ppl, epoch_val_ppl))
 
     print('Model trained.')
 
@@ -90,7 +109,7 @@ def validate(model, val_iter):
     hidden = model.init_hidden()
     aggregate_loss = []
     for batch in val_iter:
-        y_p, _ = model.forward(batch.text, hidden, train = False)
+        y_p, _ = model.forward(batch.text, hidden, train=False)
         y_t = batch.target.view(-1)
         
         loss = criterion(y_p, y_t)
@@ -101,13 +120,18 @@ def validate(model, val_iter):
 
 def main():
     # setup
+    print("Setting up...")
     args = argParser()
     device = torch.device('cuda:' + args.gpu if torch.cuda.is_available() else "cpu")
     unique_logdir = create_unique_logdir(args.log)
     logger = Logger(unique_logdir) if args.log != '' else None
+    print("All training logs will be saved to: ", unique_logdir)
+    print("Will log to tensorboard: ", logger is not None)
 
     # build TEXT object
+    print("Creating TEXT...")
     TEXT, dataset = build_dataset(args, is_parens=False)
+    print("Done!")
 
     # build model
     kwargs = vars(args) # Turns args into a dictionary
@@ -116,7 +140,8 @@ def main():
     model = model.to(device)
     
     # train model
-    train(model, dataset, TEXT, args, device, num_epochs=args.epochs)
+    print("Starting training...")
+    train(model, dataset, TEXT, args, device, num_epochs=args.epochs, logger=logger)
 
 
 if __name__ == "__main__":
