52e5970b8962496837c43e463fe09121e33d7bc5
Noa Bendit-Shtull <noa.f.bs@gmail.com>
2020-02-27 18:30:55 -0800

    [infra] Comment dataset builder code

--- dataset.py
+++ dataset.py
@@ -2,17 +2,17 @@ import torchtext
 EMBEDDING = 'glove.840B.300d'
 
 def build_dataset(args, is_parens=False):
-	"""
-	Constructs a torchtext Field and train, validation, and test datasets
-	@param args All command line arguments
-	@param is_parens Boolean, indicates whether to process parentheses dataset
+    """
+    Constructs a torchtext Field and train, validation, and test datasets
+    @param args All command line arguments
+    @param is_parens Boolean, indicates whether to process parentheses dataset
 
-	@return TEXT torchtext Field object
-	@return (train, val, test) torchtext datasets for training, validation, testing
-	"""
+    @return TEXT torchtext Field object
+    @return (train, val, test) torchtext datasets for training, validation, testing
+    """
     TEXT = torchtext.data.Field()
     if is_parens: 
-    	TEXT.tokenize = tokenize_parens
+        TEXT.tokenize = tokenize_parens
 
     # construct Dataset objects, with TEXT as the datatype
     train, val, test = torchtext.datasets.LanguageModelingDataset.splits(
@@ -27,12 +27,12 @@ def build_dataset(args, is_parens=False):
     
     # use glove embedding for word vectors 
     if not is_parens:
-    	TEXT.vocab.load_vectors(EMBEDDING)
+        TEXT.vocab.load_vectors(EMBEDDING)
 
     return TEXT, (train, val, test)
 
 def tokenize_parens(string):
-	"""
-	Tokenizer function for synthetic parenthesis dataset
-	"""
-	return string.replace("END", "").split()
+    """
+    Tokenizer function for synthetic parenthesis dataset
+    """
+    return string.replace("END", "").split()
