3148d62f9e83970517a5e640cdec142664bf5e84
Richard Lin <yifengl@stanford.edu>
2020-03-06 13:36:48 -0800

    [attention] Complete attention model

--- attentionSecondOrderLSTM.py
+++ attentionSecondOrderLSTM.py
@@ -34,19 +34,13 @@ class customCellBase(nn.Module):
                 "hidden{} has inconsistent hidden_size: got {}, expected {}".format(
                     hidden_label, hx.size(1), self.hidden_size))
 
-    def reset_parameters(self):
-        stdv = 1.0 / math.sqrt(self.hidden_size)
-        for weight in self.parameters():
-            print(weight.size())
-            weight.uniform_(-stdv, stdv)
-
 
 class AttentionSecondOrderLSTMCell(customCellBase):
     """
     For second order LSTM, we apply different weight matrix W to different inputs
     Each weight matrix W is repesented by an individual LSTMCell.
     Each LSTMCell has a corresponding attention vector V_i that is multiplied with
-    the hidden state to compute the attention score.    
+    the input embedding to compute the attention score.    
     """
     def __init__(self, vocab, second_order_size, input_size, hidden_size, bias=True, **kwargs):
         super(AttentionSecondOrderLSTMCell, self).__init__(input_size, hidden_size)
@@ -55,9 +49,9 @@ class AttentionSecondOrderLSTMCell(customCellBase):
 
         self.secondOrderLSTMCells = nn.ModuleList([nn.LSTMCell(input_size, hidden_size, bias)\
             for i in range(second_order_size)])
-        # Each cell has an attention vector V_i of size hidden_size so together they're a matrix 
-        # of size (hidden_size, second_order_size) which is essentially a linear layer with no bias
-        self.attentionScores = torch.nn.Linear(hidden_size, second_order_size, bias=False)
+        # Each cell has an attention vector V_i of size input_size(embed_size) so together they're a matrix 
+        # of size (input_size, second_order_size) which is essentially a linear layer with no bias
+        self.attentionScores = torch.nn.Linear(input_size, second_order_size, bias=False)
         self.softmax = nn.Softmax(dim=1)
             
     def temperature_softmax(self, x, temperature):
@@ -75,23 +69,22 @@ class AttentionSecondOrderLSTMCell(customCellBase):
 
     def forward(self, input, dec_states=None, temperature):
         """
-        We compute attention using the hidden state h_t. Initially the temperature should be high, so that the attention
+        We compute attention using the input embedding e_t. Initially the temperature should be high, so that the attention
         score is widely distributed. The input embedding is passed through each LSTMCell to obtain second_order_size amount
         of updated hidden states h_{t+1}. The updated hidden states are then weighted by the attention distribution and 
         summed to form a single next hidden state. As training goes on, the temperature should decrease, so the attention
         distribution would only favor one of the LSTMCell's output and the updated hidden state would effectively be the output
         hidden state of that LSTMCell.
 
-        At time sequence t, given hidden state h_t, we first compute the attention score 
-        attscore_t,i = h_t,i * V_i so that attscore_t has shape (second_order_size, ); i = {1, ... , second_order_size}
+        At time sequence t, given input embedding e_t, we first compute the attention score 
+        attscore_t,i = e_t,i * V_i so that attscore_t has shape (second_order_size, ); i = {1, ... , second_order_size}
         We then compute the attention distribution alpha_t = temperature_softmax(attscore_t)
         The attention distribution is used to weight the output hidden states of LSTMCells. We get a single updated hidden state
         h_{t+1} = \sum_{i=1}^{second_order_size} alpha_t,i * h_{t+1},i
         The cell state is updated in the same fashion where the final c_{t+1} is the weighted average of new cells states
 
         @param input Tensor(batch_size, embed_size): Input embedding of a batch for one time sequence
-        @param dec_states Tuple(Tensor(batch_size, hidden_size), *): Tuple of hidden state and cell state
-        with the same shape
+        @param dec_states Tuple(Tensor(batch_size, hidden_size), *): Tuple of hidden state and cell state with the same shape
         @param temperature [1 - 0): Should decrease as the model continues to train. See documentation on temperature_softmax above
         @return updated_states Tuple(Tensor(batch_size, hidden_size), *): Tuple of updated hidden and cell state
         """
@@ -103,11 +96,12 @@ class AttentionSecondOrderLSTMCell(customCellBase):
             cell = torch.zeros(batch_size, self.hidden_size, dtype=input.dtype, device=input.device)
             dec_states = (hidden, cell)
 
-        # (batch_size, hidden_size)
-        hidden_state, cell_state = dec_states
+        self.check_forward_hidden(input, dec_states[0], '_state')
+        self.check_forward_hidden(input, dec_states[1], '_cell')
+
         # Compute attention score of cells
         # (batch_size, second_order_size)
-        attscore = self.attentionScores(hidden_state)
+        attscore = self.attentionScores(input)
         # Compute attention distribution
         alpha = self.temperature_softmax(attscore, temperature)
 
@@ -115,27 +109,47 @@ class AttentionSecondOrderLSTMCell(customCellBase):
         updated_hidden = torch.zeros(batch_size, self.hidden_size, dtype=input.dtype, device=input.device)
         updated_cell = torch.zeros(batch_size, self.hidden_size, dtype=input.dtype, device=input.device)
         for cell_idx in range(self.second_order_size):
-            lstm_hidden, lstm_cell = self.secondOrderLSTMCells[cell_idx](input, dec_states)
-            updated_hidden = alpha[cell_idx] * lstm_hidden
-            updated_cell = alpha[cell_idx] * lstm_cell
-
-
-
-
+            updated_hidden_component, updated_cell_component = self.secondOrderLSTMCells[cell_idx](input, dec_states)
+            updated_hidden += alpha[cell_idx] * updated_hidden_component
+            updated_cell += alpha[cell_idx] * updated_cell_component
+        return (updated_hidden, updated_cell)
 
 
 class AttentionSecondOrderLSTM(nn.Module):
     """
     Second order LSTM that uses SecondOrderLSTMCell
     """
-    def __init__(self):
-        pass
+    def __init__(self, vocab, second_order_size, input_size, hidden_size, bias=True, **kwargs):
+        super(AttentionSecondOrderLSTM, self).__init__()
+        self.lstm_cell = AttentionSecondOrderLSTMCell(vocab, second_order_size, input_size, hidden_size, bias=bias)
+        self.hidden_size = hidden_size
 
-    def forward(self, input):
+    def forward(self, input, dec_states=None, temperature):
         """
         @param input Tensor(batch_size, seq_len, embed_size)
+        @param dec_states Tuple(Tensor(batch_size, hidden_size), *): Tuple of hidden state and cell state with the same shape
+        @param temperature [1 - 0): Should decrease as the model continues to train. See documentation on temperature_softmax above
+        @return combined_outputs Tensor(batch_size, seq_len, hidden_size): Output of all hidden states for each time sequence
+        @return last_dec_state: Tuple of the last hidden state and cell state in the time sequence
         """
-        pass
+        batch_size, seq_len, embed_size = input.shape
+
+        if dec_states is None:
+            hidden = torch.zeros(batch_size, self.hidden_size, dtype=input.dtype, device=input.device)
+            cell = torch.zeros(batch_size, self.hidden_size, dtype=input.dtype, device=input.device)
+            dec_states = (hidden, cell)
+
+        combined_outputs = torch.empty((batch_size, seq_len, self.hidden_size), device=input.device)
+
+        for seq_idx in range(seq_len):
+            # (batch_size, embed_size)
+            input_embed_t = input[:, seq_idx]
+            hidden, cell = self.lstm_cell(input_embed_t, dec_states, temperature)
+            dec_states = (hidden, cell)
+            combined_outputs[:, seq_idx] = hidden
+
+        return combined_outputs, (hidden, cell)
+
 
 
 # TESTs
