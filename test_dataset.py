2ef463c5fbc8fc27f8664b53350e9057e512d08d
Noa Bendit-Shtull <noa.f.bs@gmail.com>
2020-03-10 14:56:02 -0700

    [test] Upgrade PTB dataset testing
    * Remove old version of PennTreebankDataset
    * Allow testing of batches other than the first one

--- test_dataset.py
+++ test_dataset.py
@@ -3,36 +3,35 @@ from dataset import *
 def test_penntreebank():
     print ("-"*80)
     path = "data/penn/train.txt"
+
+    # CONSTANTS
     batch_size = 64
     bptt = 70    
+    words_per_batch = batch_size * bptt
+    n_batch = 199
 
+    # OPTIONS
+    n_seq = 10 # how many sequences to compare
+    batch_num = 5 # which batch to test (1 through n_batch)
+    
+    # read in first nseq*bptt words in the batch
     words = []
-    # read in first 3*bptt words in the dataset
+    start = batch_num * bptt * n_batch
+    end = start + (n_seq * bptt)
     with open(path, 'r') as file:
-        while len(words) < 3*bptt:
+        while len(words) < end:
             words += file.readline().split()
-
-    reference = " ".join(words[0:3*bptt])
-
-    print("Running Sanity Check: IterableDataset")
-    treebank1 = PennTreebankDataset(path, batch_size=batch_size, bptt=bptt)
-    vocab = Vocab(file_path="data/penn/train.json")
-    # print the first line of each batch to confirm that they are contiguous
-    words = []
-    for batch_idx, (x, y) in enumerate(treebank1):
-        words += [vocab.id2word[w] for w in x[0]]
-        if batch_idx == 2: break
-    compare1 = " ".join(words)
-    assert(compare1 == reference)
+    reference = " ".join(words[start:end])
 
     print("Running Sanity Check: Dataset")
-    treebank2 = PennTreebankDataset2(path, batch_size=batch_size, bptt=bptt)
+    treebank = PennTreebankDataset(path, batch_size=batch_size, bptt=bptt)
+    vocab = Vocab(file_path="data/penn/train.json")
     words = []
-    for i in range(0, batch_size*3, batch_size):
-        x, y = treebank2[i]
+    for i in range(0, batch_size*n_seq, batch_size):
+        x, y = treebank[i + batch_num]
         words += [vocab.id2word[w] for w in x]
-    compare2 = " ".join(words)
-    assert(compare2 == reference)
+    compare= " ".join(words)
+    assert(compare == reference)
 
     print("All Sanity Checks Passed!")
     print ("-"*80)
