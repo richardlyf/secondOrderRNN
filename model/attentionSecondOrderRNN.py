8741d7f534fda0256ddfe2486fcf7d3c3c6a4c04
Noa Bendit-Shtull <noa.f.bs@gmail.com>
2020-03-17 11:46:42 -0700

    [infra] Create separate models for RNN language models
    * Create separate base classes for RNN language models and LSTM language models
    * Create baseline RNN and attention RNN models
    * Create AttentionSecondOrderRNN layer and AttentionSecondOrderRNNCell

--- model/attentionSecondOrderRNN.py
+++ model/attentionSecondOrderRNN.py
@@ -2,51 +2,22 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import math
+from attentionSecondOrderLSTM import customCellBase
 
 
-class customCellBase(nn.Module):
+class AttentionSecondOrderRNNCell(customCellBase):
     """
-    Generic cell base that handles error checking and weight initialization
-    Based on nn.RNNCellBase: 
-    https://github.com/pytorch/pytorch/blob/ace2b4f37f26b8d7782dd6f1ce7e3738f8dc0dec/torch/nn/modules/rnn.py#L741
-    """
-    def __init__(self, input_size, hidden_size):
-        super(customCellBase, self).__init__()
-        self.input_size = input_size
-        self.hidden_size = hidden_size
-
-    def check_forward_input(self, input):
-        if input.size(1) != self.input_size:
-            raise RuntimeError(
-                "input has inconsistent input_size: got {}, expected {}".format(
-                    input.size(1), self.input_size))
-
-    def check_forward_hidden(self, input, hx, hidden_label=''):
-        # type: (Tensor, Tensor, str) -> None
-        if input.size(0) != hx.size(0):
-            raise RuntimeError(
-                "Input batch size {} doesn't match hidden{} batch size {}".format(
-                    input.size(0), hidden_label, hx.size(0)))
-
-        if hx.size(1) != self.hidden_size:
-            raise RuntimeError(
-                "hidden{} has inconsistent hidden_size: got {}, expected {}".format(
-                    hidden_label, hx.size(1), self.hidden_size))
-
-
-class AttentionSecondOrderLSTMCell(customCellBase):
-    """
-    For second order LSTM, we apply different weight matrix W to different inputs
-    Each weight matrix W is repesented by an individual LSTMCell.
-    Each LSTMCell has a corresponding attention vector V_i that is multiplied with
+    For second order RNN, we apply different weight matrix W to different inputs
+    Each weight matrix W is repesented by an individual RNNCell.
+    Each RNNCell has a corresponding attention vector V_i that is multiplied with
     the input embedding to compute the attention score.    
     """
     def __init__(self, second_order_size, input_size, hidden_size, bias=True, **kwargs):
-        super(AttentionSecondOrderLSTMCell, self).__init__(input_size, hidden_size)
+        super(AttentionSecondOrderRNNCell, self).__init__(input_size, hidden_size)
         self.hidden_size = hidden_size
         self.second_order_size = second_order_size
 
-        self.secondOrderLSTMCells = nn.ModuleList([nn.LSTMCell(input_size, hidden_size, bias)\
+        self.secondOrderRNNCells = nn.ModuleList([nn.RNNCell(input_size, hidden_size, bias)\
             for i in range(second_order_size)])
         # Each cell has an attention vector V_i of size input_size(embed_size) so together they're a matrix 
         # of size (input_size, second_order_size) which is essentially a linear layer with no bias
@@ -66,37 +37,35 @@ class AttentionSecondOrderLSTMCell(customCellBase):
         """
         return self.softmax(x / temperature)
 
-    def forward(self, input, temperature, lstm_states=None):
+    def forward(self, input, temperature, rnn_states=None):
         """
         We compute attention using the input embedding e_t. Initially the temperature should be high, so that the attention
-        score is widely distributed. The input embedding is passed through each LSTMCell to obtain second_order_size amount
+        score is widely distributed. The input embedding is passed through each RNNCell to obtain second_order_size amount
         of updated hidden states h_{t+1}. The updated hidden states are then weighted by the attention distribution and 
         summed to form a single next hidden state. As training goes on, the temperature should decrease, so the attention
-        distribution would only favor one of the LSTMCell's output and the updated hidden state would effectively be the output
-        hidden state of that LSTMCell.
+        distribution would only favor one of the RNNCell's output and the updated hidden state would effectively be the output
+        hidden state of that RNNCell.
 
         At time sequence t, given input embedding e_t, we first compute the attention score 
         attscore_t,i = e_t,i * V_i so that attscore_t has shape (second_order_size, ); i = {1, ... , second_order_size}
         We then compute the attention distribution alpha_t = temperature_softmax(attscore_t)
-        The attention distribution is used to weight the output hidden states of LSTMCells. We get a single updated hidden state
-        h_{t+1} = \sum_{i=1}^{second_order_size} alpha_t,i * h_{t+1},i
+        The attention distribution is used to weight the output hidden states of RNNCells. We get a single updated hidden state
+        h_{t+1} = sum_{i=1}^{second_order_size} alpha_t,i * h_{t+1},i
         The cell state is updated in the same fashion where the final c_{t+1} is the weighted average of new cells states
 
         @param input Tensor(batch_size, embed_size): Input embedding of a batch for one time sequence
         @param temperature [1 - 0): Should decrease as the model continues to train. See documentation on temperature_softmax above
-        @param lstm_states Tuple(Tensor(batch_size, hidden_size), *): Tuple of hidden state and cell state with the same shape
+        @param rnn_states Tuple(Tensor(batch_size, hidden_size), *): Tuple of hidden state and cell state with the same shape
         @return updated_states Tuple(Tensor(batch_size, hidden_size), *): Tuple of updated hidden and cell state
         """
         self.check_forward_input(input)
         batch_size, embed_size = input.shape
 
-        if lstm_states is None:
+        if rnn_states is None:
             hidden = torch.zeros(batch_size, self.hidden_size, dtype=input.dtype, device=input.device)
-            cell = torch.zeros(batch_size, self.hidden_size, dtype=input.dtype, device=input.device)
-            lstm_states = (hidden, cell)
+            rnn_states = hidden
 
-        self.check_forward_hidden(input, lstm_states[0], '_state')
-        self.check_forward_hidden(input, lstm_states[1], '_cell')
+        self.check_forward_hidden(input, rnn_states, '')
 
         # Compute attention score of cells
         # (batch_size, second_order_size)
@@ -104,48 +73,45 @@ class AttentionSecondOrderLSTMCell(customCellBase):
         # Compute attention distribution
         alpha = self.temperature_softmax(attscore, temperature)
 
-        # Compute updated hidden and cell state using each LSTMCell
+        # Compute updated hidden and cell state using each RNNCell
         updated_hidden = torch.zeros(batch_size, self.hidden_size, dtype=input.dtype, device=input.device)
-        updated_cell = torch.zeros(batch_size, self.hidden_size, dtype=input.dtype, device=input.device)
         for cell_idx in range(self.second_order_size):
-            updated_hidden_component, updated_cell_component = self.secondOrderLSTMCells[cell_idx](input, lstm_states)
+            updated_hidden_component = self.secondOrderRNNCells[cell_idx](input, rnn_states)
             # Add None in index to broadcast to shape (batch_size, 1)
             updated_hidden += alpha[:, cell_idx, None] * updated_hidden_component
-            updated_cell += alpha[:, cell_idx, None] * updated_cell_component
-        return (updated_hidden, updated_cell)
+        return updated_hidden
 
 
-class AttentionSecondOrderLSTM(nn.Module):
+class AttentionSecondOrderRNN(nn.Module):
     """
-    Second order LSTM that uses SecondOrderLSTMCell
+    Second order rnn that uses SecondOrderRNNCell
     """
     def __init__(self, second_order_size, input_size, hidden_size, bias=True, **kwargs):
-        super(AttentionSecondOrderLSTM, self).__init__()
-        self.lstm_cell = AttentionSecondOrderLSTMCell(second_order_size, input_size, hidden_size, bias=bias)
+        super(AttentionSecondOrderrnn, self).__init__()
+        self.rnn_cell = AttentionSecondOrderRNNCell(second_order_size, input_size, hidden_size, bias=bias)
         self.hidden_size = hidden_size
 
-    def forward(self, input, temperature, lstm_states=None):
+    def forward(self, input, temperature, rnn_states=None):
         """
         @param input Tensor(batch_size, seq_len, embed_size)
         @param temperature [1 - 0): Should decrease as the model continues to train. See documentation on temperature_softmax above
-        @param lstm_states Tuple(Tensor(batch_size, hidden_size), *): Tuple of hidden state and cell state with the same shape
+        @param rnn_states Tuple(Tensor(batch_size, hidden_size), *): Tuple of hidden state and cell state with the same shape
         @return combined_outputs Tensor(batch_size, seq_len, hidden_size): Output of all hidden states for each time sequence
-        @return last_lstm_state: Tuple of the last hidden state and cell state in the time sequence
+        @return last_rnn_state: Tuple of the last hidden state and cell state in the time sequence
         """
         batch_size, seq_len, embed_size = input.shape
 
-        if lstm_states is None:
+        if rnn_states is None:
             hidden = torch.zeros(batch_size, self.hidden_size, dtype=input.dtype, device=input.device)
-            cell = torch.zeros(batch_size, self.hidden_size, dtype=input.dtype, device=input.device)
-            lstm_states = (hidden, cell)
+            rnn_states = hidden
 
         combined_outputs = torch.empty((batch_size, seq_len, self.hidden_size), device=input.device)
 
         for seq_idx in range(seq_len):
             # (batch_size, embed_size)
             input_embed_t = input[:, seq_idx]
-            hidden, cell = self.lstm_cell(input_embed_t, temperature, lstm_states)
-            lstm_states = (hidden, cell)
+            hidden  = self.rnn_cell(input_embed_t, temperature, rnn_states)
+            rnn_states = hidden
             combined_outputs[:, seq_idx] = hidden
 
-        return combined_outputs, (hidden, cell)
+        return combined_outputs, hidden
--- model/model.py
+++ model/model.py
@@ -4,6 +4,7 @@ import torch.nn.functional as F
 from torch.autograd import Variable
 from model.paren_mLSTM import paren_mLSTM
 from model.attentionSecondOrderLSTM import AttentionSecondOrderLSTM
+from model.attentionSecondOrderRNN import AttentionSecondOrderRNN
 from model.dataset import get_glove
 
 
@@ -13,6 +14,8 @@ def ModelChooser(model_name, **kwargs):
     """
     if model_name == "baseline_lstm":
         return LSTMLanguageModel(**kwargs)
+    if model_name == "baseline_rnn":
+        return RNNLanguageModel(**kwargs)
     if model_name == "mLSTM":
         # Group by a paren and b paren
         assignments = {
@@ -41,23 +44,6 @@ class LanguageModelBase(nn.Module):
         else:
             self.hidden_shape = (num_layers, batch_size, hidden_size)
 
-    def init_lstm_state(self, device):
-        """
-        Initialize hidden state and cell state to zeros
-        """
-        zero_hidden = torch.zeros(self.hidden_shape, device=device) 
-        # zero_cell = torch.zeros(self.hidden_shape, device=device) 
-        # return (zero_hidden, zero_cell)
-        return zero_hidden 
-
-    def generate_context_state(self, device):
-        """
-        Initialize hidden state and cell state to random
-        """
-        context_hidden = torch.randn(self.hidden_shape, device=device) 
-        # context_cell = torch.randn(self.hidden_shape, device=device) 
-        return context_hidden #(context_hidden, context_cell)
-
     def detach_hidden(self, hidden):
         """
         Detaches the lstm states before returning them in forward()
@@ -68,7 +54,51 @@ class LanguageModelBase(nn.Module):
             return tuple([h.detach() for h in hidden])
 
 
-class LSTMLanguageModel(LanguageModelBase):
+class LSTMLanguageModelBase(LanguageModelBase):
+    """
+    Contains functions needed by all models for the penn tree bank dataset
+    """
+    def __init__(self, batch_size, hidden_size, num_layers=None):
+        super(LSTMLanguageModelBase, self).__init__()
+
+    def init_lstm_state(self, device):
+        """
+        Initialize hidden state and cell state to zeros
+        """
+        zero_hidden = torch.zeros(self.hidden_shape, device=device) 
+        zero_cell = torch.zeros(self.hidden_shape, device=device) 
+        return (zero_hidden, zero_cell)
+
+    def generate_context_state(self, device):
+        """
+        Initialize hidden state and cell state to random
+        """
+        context_hidden = torch.randn(self.hidden_shape, device=device) 
+        context_cell = torch.randn(self.hidden_shape, device=device) 
+        return (context_hidden, context_cell)
+
+
+class RNNLanguageModelBase(LanguageModelBase):
+    """
+    Contains functions needed by all models for the penn tree bank dataset
+    """
+    def __init__(self, batch_size, hidden_size, num_layers=None):
+        super(RNNLanguageModelBase, self).__init__()
+
+    def init_lstm_state(self, device):
+        """
+        Initialize hidden state to zeros
+        """
+        return torch.zeros(self.hidden_shape, device=device) 
+
+    def generate_context_state(self, device):
+        """
+        Initialize hidden state to random
+        """
+        return torch.randn(self.hidden_shape, device=device) 
+
+
+class LSTMLanguageModel(LSTMLanguageModelBase):
     """ simple LSTM neural network language model """     
     def __init__(self, vocab, batch_size=10, hidden_size=100, embed_size=300, dropout_rate=0.5, \
             num_layers=1, embed_path=None, device=None, **kwargs):
@@ -83,7 +113,7 @@ class LSTMLanguageModel(LanguageModelBase):
             self.embeddings.weight = nn.Parameter(glove_tensor)
             self.embeddings.weight.requires_grad = False
 
-        self.lstm = nn.RNN(
+        self.lstm = nn.LSTM(
             input_size=embed_size, 
             hidden_size=hidden_size, 
             num_layers=num_layers, 
@@ -113,7 +143,52 @@ class LSTMLanguageModel(LanguageModelBase):
         return logits, self.detach_hidden(ret_state)
 
 
-class AssignmentLanguageModel(LanguageModelBase):
+class RNNLanguageModel(RNNLanguageModelBase):
+    """ simple RNN neural network language model """     
+    def __init__(self, vocab, batch_size=10, hidden_size=100, embed_size=300, dropout_rate=0.5, \
+            num_layers=1, embed_path=None, device=None, **kwargs):
+        super(RNNLanguageModel, self).__init__(batch_size, hidden_size, num_layers)
+        
+        vocab_size = len(vocab)
+        self.embeddings = nn.Embedding(vocab_size, embed_size)
+        # load glove embeddings and initialize 
+        if embed_path is not None:
+            glove_embeddings = get_glove(embed_path, vocab)
+            glove_tensor = torch.tensor(glove_embeddings, dtype=torch.float32, device=device)
+            self.embeddings.weight = nn.Parameter(glove_tensor)
+            self.embeddings.weight.requires_grad = False
+
+        self.rnn = nn.RNN(
+            input_size=embed_size, 
+            hidden_size=hidden_size, 
+            num_layers=num_layers, 
+            batch_first=True)
+
+        self.linear = nn.Linear(in_features=hidden_size, out_features=vocab_size)
+        self.drop = nn.Dropout(p=dropout_rate)
+
+    def forward(self, x, init_state):
+        """
+        @param x: (batch_size, sequence_length)
+        @param init_state: Tuple(Tensor, Tensor)
+            each Tensor (batch_size, hidden_size)
+        """
+        # (batch_size, sequence_length, embed_size)
+        embedded = self.embeddings(x)
+        embedded = self.drop(embedded)
+        # (batch_size, sequence_length, embed_size)
+        rnn_output, ret_state = self.rnn(embedded, init_state)
+        # (batch_size * sequence_length, hidden_size)
+        reshaped = rnn_output.reshape(-1, lstm_output.size(2))
+        reshaped = self.drop(reshaped)
+        decoded = self.linear(reshaped)
+        # (batch_size * sequence_length, vocab_size)
+        logits = F.log_softmax(decoded, dim=1)
+                
+        return logits, self.detach_hidden(ret_state)
+
+
+class AssignmentLanguageModel(LSTMLanguageModelBase):
     """ second order LSTM language model with explicit assignment """     
     def __init__(self, vocab, batch_size=10, hidden_size=100, embed_size=12, dropout_rate=0.5, \
             device=None, assignments=None, num_cells=2, **kwargs):
@@ -149,7 +224,7 @@ class AssignmentLanguageModel(LanguageModelBase):
         return logits, self.detach_hidden(ret_state)
 
 
-class AttentionLanguageModel(LanguageModelBase):
+class AttentionLSTMLanguageModel(LSTMLanguageModelBase):
     """ second order attention language model """     
     def __init__(self, vocab, second_order_size=2, batch_size=10, hidden_size=100, embed_size=12, \
             device=None, temp_decay=0.9, temp_decay_interval=None, dropout_rate=0.5, **kwargs):
@@ -158,7 +233,7 @@ class AttentionLanguageModel(LanguageModelBase):
         @param temp_decay_interval: The temperature will decay after forward() is called temp_decay_interval times. Should be set to
         len(dataloader) when training. When testing, should be set to None so temperature doesn't decay.
         """
-        super(AttentionLanguageModel, self).__init__(batch_size, hidden_size)
+        super(AttentionLSTMLanguageModel, self).__init__(batch_size, hidden_size)
         
         vocab_size = len(vocab)
         self.embeddings = nn.Embedding(vocab_size, embed_size)
@@ -199,6 +274,65 @@ class AttentionLanguageModel(LanguageModelBase):
         embedded = self.drop(embedded)
         lstm_output, ret_state = self.lstm(embedded, temperature, init_state)
         reshaped = lstm_output.view(-1, lstm_output.size(2))
+        reshaped = self.drop(reshaped)
+        decoded = self.linear(reshaped)
+        # (batch_size * sequence_length, vocab_size)
+        logits = F.log_softmax(decoded, dim=1)
+                
+        return logits, self.detach_hidden(ret_state)
+
+
+
+class AttentionRNNLanguageModel(RNNLanguageModelBase):
+    """ second order attention language model """     
+    def __init__(self, vocab, second_order_size=2, batch_size=10, hidden_size=100, embed_size=12, \
+            device=None, temp_decay=0.9, temp_decay_interval=None, dropout_rate=0.5, **kwargs):
+        """
+        @param temp_decay: Everything temperature decays, the temperature is multiplied by this value
+        @param temp_decay_interval: The temperature will decay after forward() is called temp_decay_interval times. Should be set to
+        len(dataloader) when training. When testing, should be set to None so temperature doesn't decay.
+        """
+        super(AttentionRNNLanguageModel, self).__init__(batch_size, hidden_size)
+        
+        vocab_size = len(vocab)
+        self.embeddings = nn.Embedding(vocab_size, embed_size)
+
+        self.rnn = AttentionSecondOrderRNN(
+            second_order_size=second_order_size, 
+            input_size=embed_size, 
+            hidden_size=hidden_size)
+
+        self.linear = nn.Linear(in_features=hidden_size, out_features=vocab_size)
+        self.drop = nn.Dropout(p=dropout_rate)
+        
+        self.counter = 0
+        self.train_temperature = 1
+        self.test_temperature = 1e-5
+        self.temp_decay = temp_decay
+        self.temp_decay_interval = temp_decay_interval
+        
+    def forward(self, x, init_state):
+        """
+        Predict, return hidden state so it can be used to intialize the next hidden state 
+        @param x: (batch_size, sequence_length)
+        """
+        if self.training:
+            temperature = self.train_temperature
+            self.counter += 1
+            # At the end of every epoch decrease temperature
+            assert (self.temp_decay_interval is not None), "Did not set temp_decay_interval for training"
+            if self.counter == self.temp_decay_interval:
+                self.counter = 0
+                self.train_temperature *= self.temp_decay
+                print("Temperature decayed to ", self.train_temperature)
+        # For eval temperature should be set to default low
+        else:
+            temperature = self.test_temperature
+
+        embedded = self.embeddings(x)
+        embedded = self.drop(embedded)
+        rnn_output, ret_state = self.rnn(embedded, temperature, init_state)
+        reshaped = rnn_output.view(-1, lstm_output.size(2))
         reshaped = self.drop(reshaped)
         decoded = self.linear(reshaped)
         # (batch_size * sequence_length, vocab_size)
